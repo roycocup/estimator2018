8c54c807d39844fdd23ed228bdec15da0ed223ee
Rodrigo Dias <rodrigo.dias@browsergroup.com>
2018-12-05 21:33:11 +0000

    removing lumen site

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 service  apache2 stop;
 
 # nginx -g 'daemon off;'
+# php -S 0.0.0.0:8888 -t site/public
 
-php -S 0.0.0.0:8888 -t site/public
+php -S 0.0.0.0:8888 
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+# composer global require "laravel/lumen-installer"
 # lumen new site
 
+# composer create-project symfony/website-skeleton my_project
 
 docker-compose -f "docker-compose.yml" down && \
 docker-compose -f "docker-compose.yml" up --build
